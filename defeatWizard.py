091c651ba4332999c72822140264b7859242ab3a
Tyler Wronski <wronskityler@gmail.com>
2025-05-22 09:58:03 -0700

    Added 'Archer' and 'Paladin' characters. Started Character Abilities

--- defeatWizard.py
+++ defeatWizard.py
@@ -21,17 +21,33 @@ class Warrior(Character):
     def __init__(self, name):
         super().__init__(name, health=140, attack_power=25)
         
+    # Special Ability: Rage (bonus damage)
+    def special1(self):
+        pass 
+    
+    # Special Ability: Swift Feet (dodge attack)
+    def special2(self):
+        pass 
+        
 
 # Mage class (inherits from Character)
 class Mage(Character):
     def __init__(self, name):
         super().__init__(name, health=100, attack_power=35)
+        
+    # Special Ability: Spell Book (extra heal)
+    def special1(self): 
+        pass 
+    
+    # Special Ability: Magical Force (blocks attack)
+    def special2(self):
+        pass 
 
 
 # EvilWizard class (inherits from Character)
 class EvilWizard(Character):
     def __init__(self, name):
-        super().__init__(name, health=150, attack_power=15)
+        super().__init__(name, health=175, attack_power=15)
 
     def regenerate(self):
         self.health += 5
@@ -39,10 +55,30 @@ class EvilWizard(Character):
 
 
 # Create Archer class
-
+class Archer(Character):
+    def __init__(self, name):
+        super().__init__(name, health=100, attack_power=20)
+        
+    # Special Ability: Double arrow attack
+    def special1(self):
+        pass 
+    
+    # Evade, dodges next attack 
+    def special2(self):
+        pass 
 
 # Create Paladin class 
-
+class Paladin(Character):
+    def __init__(self, name):
+        super().__init__(name, health=150, attack_power=15)
+        
+    # Special Ablility: Holy Strike (bonus damage)
+    def special1(self):
+        pass 
+    
+    # Special Ability: Divine Shield (blocks next attack) 
+    def special2(self):
+        pass 
 
 
 def create_character():
@@ -60,9 +96,9 @@ def create_character():
     elif class_choice == '2':
         return Mage(name)
     elif class_choice == '3':
-        pass  # Implement Archer class
+        return Archer(name) 
     elif class_choice == '4':
-        pass  # Implement Paladin class
+        return Paladin(name)
     else:
         print("Invalid choice. Defaulting to Warrior.")
         return Warrior(name)
@@ -81,6 +117,17 @@ def battle(player, wizard):
             player.attack(wizard)
         elif choice == '2':
             pass  # Implement special abilities
+            print("\n--- Special Abilities ---") 
+            print("1. Damage Boost")
+            print("2. Dodge Attack") 
+            ability = input("Enter the number of the special ability you wish to use: ")
+            if ability == '1':
+                player.special1()
+            elif ability == '2':
+                player.special2()
+            else:
+                print("Invalid Choice. Try again.")
+            
         elif choice == '3':
             pass  # Implement heal method
         elif choice == '4':
